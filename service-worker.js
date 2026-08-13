6c3bd7dfa54f3d6cbb965badb4da1d622ffcaa6f
OTalDoug <sou.dougsoares@gmail.com>
2026-08-12 22:45:31 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'diario-online-v7-2-fast-restore';
+const CACHE_VERSION = 'diario-online-v7-3-top-postmessage';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 
 const APP_SHELL = [
